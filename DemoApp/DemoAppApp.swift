efe6826c492166d110eed4a89ffad94ca003839a
surajForDD <surajkm09@gmail.com>
2023-06-26 12:37:34 +0530

    Initial Commit

---
+++ DemoApp/DemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by suraj kumar on 26/06/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
