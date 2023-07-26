9eb3bb691bd842e7a72fdcd3461cb003d995d895
surajForDD <surajkm09@gmail.com>
2023-07-26 13:25:57 +0530

    Initial commit

--- DemoApp/ContentView.swift
+++ DemoApp/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var userName :String = ""
+    @State private var password :String = ""
+    @State private var  showLoginView:Bool = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+            //            Image(systemName: "globe")
+            //                .imageScale(.large)
+            //                .foregroundColor(.accentColor)
+            //            Text("Hello, world!")
+            
+
+        
+           
+             
+            if showLoginView {
+                            HomeView()
+                        } else {
+                            
+                            TextField("User Name",text: $userName)
+                            TextField("password",text: $password)
+                            Button("Login") {
+                                self.showLoginView = true
+                            }
+                        }
+                    
+            
+        }.padding()
     }
 }
 
